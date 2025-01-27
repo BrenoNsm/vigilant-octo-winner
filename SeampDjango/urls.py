0d9250e9fe5461279e8cbee581eac39289de2754
Breno Nascimento <brenonsmacedo@gmail.com>
2025-01-27 19:09:18 -0400

    allowed_host

--- SeampDjango/urls.py
+++ SeampDjango/urls.py
@@ -23,6 +23,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
         path('admin/', admin.site.urls),
-        path('', include('siteSeamp.urls'))
+       # path('', include('siteSeamp.urls'))
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
